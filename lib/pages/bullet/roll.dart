a2515dd79e67f0736c31dbf09956f42421a8c904
webkonglong <anmingzhe@me.com>
2020-11-28 00:09:04 +0800

    [feat]

--- lib/pages/bullet/roll.dart
+++ lib/pages/bullet/roll.dart
@@ -17,38 +17,72 @@ class Roll extends StatefulWidget {
 class _Roll extends State<Roll> with TickerProviderStateMixin<Roll> {
   ScrollController _controller = ScrollController();
 
+  Timer outtTime;
+
+  void rollFn() {
+    if (_controller != null) {
+      _controller.jumpTo(_controller.position.minScrollExtent);
+      _controller.animateTo(_controller.position.maxScrollExtent,
+          duration: Duration(milliseconds: 4000), curve: Curves.linear);
+    }
+  }
+
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIOverlays([]);
-
     OrientationPlugin.forceOrientation(DeviceOrientation.landscapeLeft);
 
-    Timer(Duration(milliseconds: 500), () {
-      // _controller.jumpTo(_controller.position.maxScrollExtent, duration: Duration(milliseconds: 200));
-      _controller.animateTo(
-        _controller.position.maxScrollExtent,
-        // 返回顶部的过程中执行一个滚动动画，动画时间是200毫秒，动画曲线是Curves.ease
-        duration: Duration(milliseconds: 5000),
-        curve: Curves.ease,
-      );
+    Timer(Duration(milliseconds: 200), () {
+      rollFn();
+    });
+
+    outtTime = Timer.periodic(Duration(milliseconds: 4000), (timer) {
+      rollFn();
     });
 
     return Scaffold(
         backgroundColor: Color(widget.backgroundColor),
         body: Container(
           alignment: Alignment.center,
-          child: ListView(
-              controller: _controller,
-              scrollDirection: Axis.horizontal,
-              children: [
-                Text(
-                  widget.text,
-                  style: TextStyle(
-                      color: Color(widget.textColor),
-                      fontWeight: FontWeight.bold,
-                      wordSpacing: Px.px(750),
-                      fontSize: Px.px(600)),
+          child: Stack(
+            children: [
+              ListView(
+                  controller: _controller,
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    Container(
+                      padding:
+                          EdgeInsets.fromLTRB(Px.screenH(), 0, Px.screenH(), 0),
+                      child: Text(
+                        widget.text,
+                        style: TextStyle(
+                            color: Color(widget.textColor),
+                            fontWeight: FontWeight.bold,
+                            wordSpacing: Px.px(750),
+                            fontSize: Px.px(600)),
+                      ),
+                    )
+                  ]),
+              Positioned(
+                top: Px.px(30),
+                left: Px.px(30),
+                child: GestureDetector(
+                  child: Image.asset(
+                    "images/while_out.png",
+                    height: Px.px(30),
+                    fit: BoxFit.contain,
+                  ),
+                  onTap: () {
+                    outtTime.cancel();
+                    outtTime = null;
+                    _controller = null;
+                    OrientationPlugin.forceOrientation(
+                        DeviceOrientation.portraitUp);
+                    Navigator.of(context).pop();
+                  },
                 ),
-              ]),
+              )
+            ],
+          ),
         ));
   }
 }
